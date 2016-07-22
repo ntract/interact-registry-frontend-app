54300028f9b8829c4d9b05257ae4dbd0fa7f819b
Oliver Foster <oliver.foster@kineo.com>
2016-07-22 15:41:47 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -1,49 +1,49 @@
-plugins.started(function() {
 require([
 	"plugins/interact-registry-frontend-app/js/content",
 	"plugins/interact-registry-frontend-app/js/header",
 	"backbone.differ"
 ], function(ContentView, HeaderView) {
 
-	var MainView = Backbone.DiffView.extend({
+	plugins.started(function() {
+		var MainView = Backbone.DiffView.extend({
 
-		className: "main",
-		attributes: {
-			"view-container": true
-		},
+			className: "main",
+			attributes: {
+				"view-container": true
+			},
 
-		postInitialize: function(options) {
-			this.render();
-		},
+			postInitialize: function(options) {
+				this.render();
+			},
 
-		postRender: function(isFirstRender) {
-			if (!isFirstRender) return;
+			postRender: function(isFirstRender) {
+				if (!isFirstRender) return;
 
-			this.addChildren();
-		},
+				this.addChildren();
+			},
 
-		addChildren: function() {
-			this.header = new HeaderView({
-				parent: this
-			});
-			this.content = new ContentView({
-				parent: this
-			});
-			this.$el.append(this.header.$el);
-			this.$el.append(this.content.$el);
-		}
+			addChildren: function() {
+				this.header = new HeaderView({
+					parent: this
+				});
+				this.content = new ContentView({
+					parent: this
+				});
+				this.$el.append(this.header.$el);
+				this.$el.append(this.content.$el);
+			}
 
 
-	}, {
-		template: "main"
+		}, {
+			template: "main"
+		});
+
+
+		window.app = {
+			main: (new MainView({}))
+		};
+
+		$("body").append(app.main.$el);
+
 	});
-
-
-	window.app = {
-		main: (new MainView({}))
-	};
-
-	$("body").append(app.main.$el);
-
-});
 });
